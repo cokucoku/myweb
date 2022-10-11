9eb5a90c115b4728a9408b60969fe0b68aee42b1
lee <34832462@qq.com>
2022-10-11 17:29:22 +0800

    T

--- js/loadImg.js
+++ js/loadImg.js
@@ -17,20 +17,26 @@ let loadImg = function (option) {
         onLoad: ''
     }
     const myOption = Object.assign(setOption, option)
-    let count = 0, len = myOption.imgs.length
+    let count = 0, len = myOption.imgs.length, bai = 0
     load();
 
     function load() {
+        let st = setInterval(() => {
+            myOption.onLoad.call(that, bai)
+            bai++
+        }, 100)
         let img = new Image()
         img.onload = () => {
-            let bai=(count / len * 100).toFixed()
+            bai = (count / len * 100).toFixed()
             myOption.onLoad && myOption.onLoad.call(that, bai)
             if (count >= len) {
+                clearInterval(st)
                 myOption.onComplete.call(that, 'ok')
             } else {
                 load()
             }
             count++
+            clearInterval(st)
         }
         img.src = myOption.imgs[count]
     }
